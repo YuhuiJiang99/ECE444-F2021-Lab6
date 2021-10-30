55979c048f06b1026a9e7134c950bda46dc4f1ce
YuhuiJiang99 <yuhui.jiang@outlook.com>
2021-10-29 20:33:10 -0400

    modify app.py

--- project/app.py
+++ project/app.py
@@ -31,6 +31,8 @@ SQLALCHEMY_DATABASE_URI = os.getenv(
 )
 SQLALCHEMY_TRACK_MODIFICATIONS = False
 
+if SQLALCHEMY_DATABASE_URI and SQLALCHEMY_DATABASE_URI .startswith("postgres://"):
+    SQLALCHEMY_DATABASE_URI  = SQLALCHEMY_DATABASE_URI .replace("postgres://", "postgresql://", 1)
 
 # create and initialize a new Flask app
 app = Flask(__name__)
